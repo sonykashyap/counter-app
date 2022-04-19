1eced507a80c0df6608fcd3e9e5a8332c0b07926
Sony <58991617+sonykashyap@users.noreply.github.com>
2022-04-19 16:59:59 +0530

    Added loader and improved counter component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 // import logo from './logo.svg';
-import { Component } from 'react';
+import React from 'react';
 import Counter from './components/counter';
-
+import Loader from './components/loader';
+import axios from 'axios';
 
 import './App.css';
 
-class App extends Component {
-  state = {
-    btn_color : "button-red",
-    name      : "Counter-app",
-    counterValue: 1
-  };
-  
-  onClick = (event) =>{
-    this.setState({
-      btn_color:'button-grey'
-    });
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {counterValue: 1, isCounter: false, isLoader: false};
+    axios.get("https://interview-8e4c5-default-rtdb.firebaseio.com/front-end/counter1.json")
+    .then(response =>{
+      if(response.data){
+        this.setState({isCounter:true});
+        if('debanjan' in response.data)
+        // this.setState({isCounter:true});
+        this.setState({counterValue: response.data.debanjan});
+      }else{
+        this.setState({isCounter:true});
+      }
+    })
+  }
+  handleCallBack = (childData) =>{
+    console.log("Child data is ", childData);
+    this.setState({counterValue: childData});
   }
   render() {
     return(
       <div className="App">
-      <span className='saved-text'>Saving Counter value</span>
-        <Counter value={this.state.counterValue}></Counter>
+      {
+        this.state.isLoader ? <div className='Loader-container'>
+          <Loader ></Loader>
+          <span className='saved-text'>Saving Counter value</span>
+        </div> : ''
+      }
+        
+        <Counter parentCallBack={this.handleCallBack} ></Counter>
         <div>Counter value: { this.state.counterValue }</div>
       </div>
     )
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,28 +1,70 @@
-import React,{ Component } from 'react';
+import React from 'react';
 
-class Counter extends Component{
-    state = {
-        counterValue : this.props.value
+class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {counterValue: 1, isLoader: false};
+        console.log("State is ", this.state.counterValue);
+        this.handleChange = this.handleChange.bind(this);
+
     }
-    valueCounter = () =>{
-        console.log("Value is ", this.state.counterValue);
-        this.setState({
-            counterValue : this.state.counterValue + 1,
-        })
+    componentDidMount(){
+        // console.log("Yes Counted component mounted");
+        this.handleChange();
+        
     }
-    valueDecrement = () => {
-        console.log("Value decrement function()", this.state.counterValue - 1);
-        this.setState({
-           counterValue: this.state.counterValue - 1,
-        })
-        // console.log("Value is ", this.state.counterValue);
+    handleChange = (event) =>{
+        if(event === undefined){
+            this.props.parentCallBack(this.state.counterValue);
+        }else{
+            console.log("IF else part");
+            if(event.target.id === "counterIncrement"){
+                this.setState({
+                    counterValue : this.state.counterValue + 1,
+                });
+                this.counterValueChange(this.state.counterValue);
+
+            } else if(event.target.id === "counterDecrement"){
+                this.setState({
+                    counterValue: this.state.counterValue - 1,
+                });
+                this.counterValueChange(this.state.counterValue);
+
+            } else{
+                this.setState({counterValue : event.target.value});
+                // this.props.parentCallBack(event.target.value, this.state.isLoader);
+                this.counterValueChange(this.state.counterValue);
+                event.preventDefault();
+            }
+            
+        }
+        
     }
+    counterValueChange = (value) =>{
+        // this.setState({counterValue : event.target.value});
+        console.log("Valuers is ", value);
+        this.props.parentCallBack(value);
+    }
+    // valueCounter = (event) =>{
+    //     console.log("event target is ", event.target.id);
+    //     this.setState({
+    //         counterValue : this.state.counterValue + 1,
+    //     })
+    //     // console.log("Changed  value is ", this.state.counterValue);
+    // }
+    // valueDecrement = (event) => {
+    //     console.log("event target is ", event.target.id);
+    //     this.setState({
+    //        counterValue: this.state.counterValue - 1,
+    //     })
+    // }
+    
     render(){
         return(
             <div className="counter-container">
-                <button className='btn-decrement' onClick={this.valueDecrement}> - </button>
-                <input className='Counter-value' value={this.state.counterValue}/>
-                <button className='btn-increment' onClick={ this.valueCounter }> + </button>
+                <button className='btn-decrement' id="counterDecrement" onClick={ this.handleChange} > - </button>
+                <input className='Counter-value' value={ this.state.counterValue} onChange={this.handleChange} />
+                <button className='btn-increment' id="counterIncrement" onClick={ this.handleChange }> + </button>
             </div>
         )
     }
